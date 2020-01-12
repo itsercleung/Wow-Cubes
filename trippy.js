b832de4270d3caec1a5ef911eff565f4da34e970
itsercleung <leung.eric24@gmail.com>
2020-01-12 19:46:29 +1300

    Added stroke weight and buttons!

--- trippy.js
+++ trippy.js
@@ -1,5 +1,7 @@
 //Using j5 JS and WEBGL
-//TODO: Implement sliders
+//TODO: Fix up code - Very hard coded
+
+p5.disableFriendlyErrors = true; // disables FES
 
 //Customizable variables
 let angle = 0;
@@ -14,7 +16,6 @@ let offsetAngleX = 0;
 let offsetAngleY = 0;
 
 let oscillateValue = 0;
-
 let speed = 0.05;
 let reverse = true;
 
@@ -25,14 +26,17 @@ let brightness = 255;
 let dlr = 255;
 let dlg = 120;
 let dlb = 110;
+let stroke = 0;
+
 
 //-----------------------------------------------
 
 let isometricAngle;
 let maxDistance;
-let densitySlider, speedSlider, xAngleSlider, yAngleSlider, oscillateSlider, maxDistanceSlider, dlrSlider, dlgSlider, dlbSlider, 
-rSlider, gSlider, bSlider, brightnessSlider;
-p5.disableFriendlyErrors = true; // disables FES
+let densitySlider, speedSlider, xAngleSlider, yAngleSlider, oscillateSlider, 
+maxDistanceSlider, dlrSlider, dlgSlider, dlbSlider, 
+rSlider, gSlider, bSlider, brightnessSlider, strokeSlider; 
+let reverseButton, defaultButton;
 
 function windowResized() {
   resizeCanvas(windowHeight, windowHeight);
@@ -73,51 +77,91 @@ function setup() {
   maxDistanceSlider.addClass("slider");
 
   rSlider = createSlider(0, 255, r, 1);
-  rSlider.position(100, 260);
+  rSlider.position(100, 230);
   rSlider.addClass("slider");
 
   gSlider = createSlider(0, 255, g, 1);
-  gSlider.position(100, 290);
+  gSlider.position(100, 260);
   gSlider.addClass("slider");
 
   bSlider = createSlider(0, 255, b, 1);
-  bSlider.position(100, 320);
+  bSlider.position(100, 290);
   bSlider.addClass("slider");
 
   brightnessSlider = createSlider(0, 255, brightness, 1);
-  brightnessSlider.position(100, 350);
+  brightnessSlider.position(100, 320);
   brightnessSlider.addClass("slider");
 
   dlrSlider = createSlider(0, 255, dlr, 1);
-  dlrSlider.position(100, 380);
+  dlrSlider.position(100, 350);
   dlrSlider.addClass("slider");
 
   dlgSlider = createSlider(0, 255, dlg, 1);
-  dlgSlider.position(100, 410);
+  dlgSlider.position(100, 380);
   dlgSlider.addClass("slider");
 
   dlbSlider = createSlider(0, 255, dlb, 1);
-  dlbSlider.position(100, 440);
+  dlbSlider.position(100, 410);
   dlbSlider.addClass("slider");
 
-  setupText();
+  strokeSlider = createSlider(0, 25, stroke, 1);
+  strokeSlider.position(100, 440);
+  strokeSlider.addClass("slider");
 
+  //Button setup
+  reverseButton = createButton('Reverse');
+  reverseButton.position(10, 490);
+  reverseButton.mousePressed(reverseClicked);
+  reverseButton.addClass("button");
+
+  defaultButton = createButton('Reset');
+  defaultButton.position(115, 490);
+  defaultButton.mousePressed(resetClicked);
+  defaultButton.addClass("button");
+
+  //Label text positioning
+  setupText();
 }
 
 //Render objects and lighting
 function draw() {
-  //Canvas lighting render
-  noStroke();
   background(255);
-  ortho(-700, 700, 700, -700, 0, 2000); //persective camera angle
-  directionalLight(dlr, dlg, dlb, -0.5, -0.5, 0); //lighting 
+  ortho(-700, 700, 700, -700, 0, 2000); //canvas perspective
+
+  //Lighting
+  directionalLight(dlr, dlg, dlb, -0.5, -0.5, 0);
   ambientLight(brightness);
   ambientMaterial(r, g, b);
+
+  //Stroke of cube
+  strokeWeight(stroke);
 
   //Isometric view angle
   rotateX(isometricAngle + offsetAngleX);
   rotateY(-QUARTER_PI + offsetAngleY);
 
+  //Draw cubes oscillating
+  drawCubes();
+
+  //Sliders value change
+  w = densitySlider.value();
+  speed = speedSlider.value();
+  offsetAngleX = xAngleSlider.value();
+  offsetAngleY = yAngleSlider.value();
+  oscillateValue = oscillateSlider.value();
+  maxDistance = maxDistanceSlider.value();
+  r = rSlider.value();
+  g = gSlider.value();
+  b = bSlider.value();
+  brightness = brightnessSlider.value();
+  dlr = dlrSlider.value();
+  dlg = dlgSlider.value();
+  dlb = dlbSlider.value();
+  stroke = strokeSlider.value();
+
+}
+
+function drawCubes() {
   //Draw x number of times
   for (let z = 0; z < height; z += w) { //row
     for (let x = 0; x < width; x += w) { //col 
@@ -140,21 +184,6 @@ function draw() {
   } else {
     angle -= speed;
   }
-
-  //Sliders value change
-  w = densitySlider.value();
-  speed = speedSlider.value();
-  offsetAngleX = xAngleSlider.value();
-  offsetAngleY = yAngleSlider.value();
-  oscillateValue = oscillateSlider.value();
-  maxDistance = maxDistanceSlider.value();
-  r = rSlider.value();
-  g = gSlider.value();
-  b = bSlider.value();
-  brightness = brightnessSlider.value();
-  dlr = dlrSlider.value();
-  dlg = dlgSlider.value();
-  dlb = dlbSlider.value();
 }
 
 function setupText() {
@@ -186,31 +215,59 @@ function setupText() {
 
   var rText = document.getElementById("rText");
   rText.style.left = rSlider.x * 1.4 - rSlider.width + "px";
-  rText.style.top = "245px";
+  rText.style.top = "215px";
 
   var gText = document.getElementById("gText");
   gText.style.left = gSlider.x * 1.4 - gSlider.width + "px";
-  gText.style.top = "275px";
+  gText.style.top = "245px";
 
   var bText = document.getElementById("bText");
   bText.style.left = bSlider.x * 1.4 - bSlider.width + "px";
-  bText.style.top = "305px";
+  bText.style.top = "275px";
 
   var brightnessText = document.getElementById("brightnessText");
   brightnessText.style.left = brightnessSlider.x * 1.4 - brightnessSlider.width + "px";
-  brightnessText.style.top = "335px";
+  brightnessText.style.top = "305px";
 
   var dlrText = document.getElementById("dlrText");
   dlrText.style.left = dlrSlider.x * 1.4 - dlrSlider.width + "px";
-  dlrText.style.top = "365px";
+  dlrText.style.top = "335px";
 
   var dlgText = document.getElementById("dlgText");
   dlgText.style.left = dlgSlider.x * 1.4 - dlgSlider.width + "px";
-  dlgText.style.top = "395px";
+  dlgText.style.top = "365px";
 
   var dlbText = document.getElementById("dlbText");
   dlbText.style.left = dlbSlider.x * 1.4 - dlbSlider.width + "px";
-  dlbText.style.top = "425px";
+  dlbText.style.top = "395px";
+
+  var strokeText = document.getElementById("strokeText");
+  strokeText.style.left = strokeSlider.x * 1.4 - strokeSlider.width + "px";
+  strokeText.style.top = "425px";
+}
+
+function reverseClicked() {
+  reverse = !reverse;
+}
+
+//Uh oh
+function resetClicked() {
+  densitySlider.value(40);
+  speedSlider.value(0.05);
+  xAngleSlider.value(0); 
+  yAngleSlider.value(0);
+  oscillateSlider.value(0);
+  maxDistanceSlider.value(dist(0, 0, 400, 400));
+  rSlider.value(220);
+  gSlider.value(180);
+  bSlider.value(80);
+  brightnessSlider.value(255);
+  dlrSlider.value(255);
+  dlgSlider.value(120);
+  dlbSlider.value(110);
+  strokeSlider.value(0);
+
+  reverse = false;
 }
 
 
