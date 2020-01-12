593c496e34e7b8bd62f861742a52fb16d3dd4fef
itsercleung <leung.eric24@gmail.com>
2020-01-12 13:43:16 +1300

    Added labels and a links

--- trippy.js
+++ trippy.js
@@ -7,8 +7,8 @@ let w = 40; //width of rendered shapes (num shapes)
 
 let distX1 = 0;
 let distY1 = 0;
-let distX2 = 200; //distance of height offset
-let distY2 = 200; 
+let distX2 = 400; //distance of height offset
+let distY2 = 400; 
 
 let offsetAngleX = 0;
 let offsetAngleY = 0;
@@ -18,13 +18,13 @@ let oscillateValue = 0;
 let speed = 0.05;
 let reverse = true;
 
-let r = 120;
-let g = 190;
-let b = 110;
+let r = 220;
+let g = 180;
+let b = 80;
 let brightness = 255;
 let dlr = 255;
-let dlg = 0;
-let dlb = 0;
+let dlg = 120;
+let dlb = 110;
 
 //-----------------------------------------------
 
@@ -34,50 +34,62 @@ let densitySlider, speedSlider, xAngleSlider, yAngleSlider, oscillateSlider, max
 rSlider, gSlider, bSlider, brightnessSlider;
 p5.disableFriendlyErrors = true; // disables FES
 
+function windowResized() {
+  resizeCanvas(windowHeight, windowHeight);
+}
+
+//Setup canvas and objects
 function setup() {
-  createCanvas(700, 700, WEBGL);
+   var canvas = createCanvas(windowHeight, windowHeight, WEBGL);
+  canvas.style('display', 'block');
+
+  //Define angle and distance of rendering cubes
   isometricAngle = atan(1 / sqrt(2));
   maxDistance = dist(distX1, distY1, distX2, distY2);
 
   // create sliders
   densitySlider = createSlider(10, 100, w, 1);
-  densitySlider.position(20, 20);
+  densitySlider.position(100, 20);
   speedSlider = createSlider(0, 0.2, speed, 0.001);
-  speedSlider.position(20, 50);
+  speedSlider.position(100, 50);
 
   xAngleSlider = createSlider(-1, 1, offsetAngleX, 0.01);
-  xAngleSlider.position(20, 80);
+  xAngleSlider.position(100, 80);
   yAngleSlider = createSlider(-1, 1, offsetAngleY, 0.01);
-  yAngleSlider.position(20, 110);
+  yAngleSlider.position(100, 110);
 
   oscillateSlider = createSlider(-1, 1, oscillateValue, 0.01);
-  oscillateSlider.position(20, 140);
+  oscillateSlider.position(100, 140);
 
   maxDistanceSlider = createSlider(1, 1000, maxDistance, 10);
-  maxDistanceSlider.position(20, 170);
+  maxDistanceSlider.position(100, 170);
 
   rSlider = createSlider(0, 255, r, 1);
-  rSlider.position(600, 20);
+  rSlider.position(100, 260);
 
   gSlider = createSlider(0, 255, g, 1);
-  gSlider.position(600, 50);
+  gSlider.position(100, 290);
 
   bSlider = createSlider(0, 255, b, 1);
-  bSlider.position(600, 80);
+  bSlider.position(100, 320);
 
   brightnessSlider = createSlider(0, 255, brightness, 1);
-  brightnessSlider.position(600, 110);
+  brightnessSlider.position(100, 350);
 
   dlrSlider = createSlider(0, 255, dlr, 1);
-  dlrSlider.position(600, 140);
+  dlrSlider.position(100, 380);
 
   dlgSlider = createSlider(0, 255, dlg, 1);
-  dlgSlider.position(600, 170);
+  dlgSlider.position(100, 410);
 
   dlbSlider = createSlider(0, 255, dlb, 1);
-  dlbSlider.position(600, 200);
+  dlbSlider.position(100, 440);
+
+  setupText();
+
 }
 
+//Render objects and lighting
 function draw() {
   //Canvas lighting render
   noStroke();
@@ -128,10 +140,62 @@ function draw() {
   dlr = dlrSlider.value();
   dlg = dlgSlider.value();
   dlb = dlbSlider.value();
+}
 
-  //text('red', rSlider.x * 2 + rSlider.width, 35);
-  //text('green', gSlider.x * 2 + gSlider.width, 65);
-  //text('blue', bSlider.x * 2 + bSlider.width, 95);
+function setupText() {
+  var densityText = document.getElementById("densityText");
+  densityText.style.left = densitySlider.x * 1.4 - densitySlider.width + "px";
+  densityText.style.top = "5px";
+
+  var speedText = document.getElementById("speedText");
+  speedText.style.left = speedSlider.x * 1.4 - speedSlider.width + "px";
+  speedText.style.top = "35px";
+
+  var offsetAngleXText = document.getElementById("offsetAngleXText");
+  offsetAngleXText.style.left = xAngleSlider.x * 1.4 - xAngleSlider.width + "px";
+  offsetAngleXText.style.top = "65px";
+
+  var offsetAngleYText = document.getElementById("offsetAngleYText");
+  offsetAngleYText.style.left = yAngleSlider.x * 1.4 - yAngleSlider.width + "px";
+  offsetAngleYText.style.top = "95px";
+
+  var oscillateText = document.getElementById("oscillateText");
+  oscillateText.style.left = oscillateSlider.x * 1.4 - oscillateSlider.width + "px";
+  oscillateText.style.top = "125px";
+
+  var maxDistanceText = document.getElementById("maxDistanceText");
+  maxDistanceText.style.left = maxDistanceSlider.x * 1.4 - maxDistanceSlider.width + "px";
+  maxDistanceText.style.top = "155px";
+
+  //------
+
+  var rText = document.getElementById("rText");
+  rText.style.left = rSlider.x * 1.4 - rSlider.width + "px";
+  rText.style.top = "245px";
+
+  var gText = document.getElementById("gText");
+  gText.style.left = gSlider.x * 1.4 - gSlider.width + "px";
+  gText.style.top = "275px";
+
+  var bText = document.getElementById("bText");
+  bText.style.left = bSlider.x * 1.4 - bSlider.width + "px";
+  bText.style.top = "305px";
+
+  var brightnessText = document.getElementById("brightnessText");
+  brightnessText.style.left = brightnessSlider.x * 1.4 - brightnessSlider.width + "px";
+  brightnessText.style.top = "335px";
+
+  var dlrText = document.getElementById("dlrText");
+  dlrText.style.left = dlrSlider.x * 1.4 - dlrSlider.width + "px";
+  dlrText.style.top = "365px";
+
+  var dlgText = document.getElementById("dlgText");
+  dlgText.style.left = dlgSlider.x * 1.4 - dlgSlider.width + "px";
+  dlgText.style.top = "395px";
+
+  var dlbText = document.getElementById("dlbText");
+  dlbText.style.left = dlbSlider.x * 1.4 - dlbSlider.width + "px";
+  dlbText.style.top = "425px";
 }
 
 
